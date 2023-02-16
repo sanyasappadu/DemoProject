3ad6ab61542e5380443089c7ecff2a816eef8c8c
Naidu <naidu@Naidus-MacBook-Air.local>
2023-02-16 23:48:28 +0530

    first commmit

---
+++ web.js
@@ -0,0 +1,70 @@
+const input = {
+    products: {
+        lite: {
+            id: "lite",
+            name: "Lite",
+            description: "Lite plan description",
+            price: 99,
+        },
+        premium: {
+            id: "premium",
+            name: "Premium",
+            description: "Premium plan description",
+            price: 199,
+        },
+        ultra: {
+            id: "ultra",
+            name: "Ultra",
+            description: "Ultra plan description",
+            price: 299
+        }
+    },
+    plans: {
+        monthly: ["lite", "ultra"],
+        yearly: ["lite", "premium"]
+    }
+}
+const output2 = {}
+Object.keys(input.products).forEach((product) => {
+    output2[product] = {}
+    Object.keys(input.plans).forEach((plan)=>{
+        console.log(input.plans[plan].find((x) => x === product));
+        output2[product][plan]={}
+        console.log(input.plans[plan]);
+    })
+})
+console.log(output2);
+
+// output
+// {
+//     lite: {
+//         monthly: {
+//             id: "lite",
+//             name: "Lite",
+//             description: "Lite plan description",
+//             price: 99,
+//         },
+//         yearly: {
+//             id: "lite",
+//             name: "Lite",
+//             description: "Lite plan description",
+//             price: 99,
+//         }
+//     },
+//     premium: {
+//         yearly: {
+//             id: "premium",
+//             name: "Premium",
+//             description: "Premium plan description",
+//             price: 199,
+//         }
+//     },
+//     ultra: {
+//         monthly: {
+//             id: "ultra",
+//             name: "Ultra",
+//             description: "Ultra plan description",
+//             price: 299
+//         }
+//     }
+// }
